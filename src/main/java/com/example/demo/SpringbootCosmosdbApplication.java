7898e771183c0b69b48b1f947856e5af2be82e9d
jj152 <ranadipdas.gcect@gmail.com>
2023-06-16 02:25:06 +0530

    initial commit

---
+++ src/main/java/com/example/demo/SpringbootCosmosdbApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCosmosdbApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(SpringbootCosmosdbApplication.class, args);
+   }
+
+}
